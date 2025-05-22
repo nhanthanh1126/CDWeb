1d7b61618f9899a5dc7e4dbbe48bebd59dddc6af
Truong Thanh Nhan <91606855+nhanthanh1126@users.noreply.github.com>
2025-05-22 23:31:51 +0700

    Hoàn Thiện Giao Diện (Còn Lại)

--- Frontend/src/containers/App.js
+++ Frontend/src/containers/App.js
@@ -44,7 +44,7 @@ class App extends Component {
             <Fragment>
                 <Router history={history}>
                     <div className="main-container">
-                        {this.props.isLoggedIn && <Header />}
+                        {/* {this.props.isLoggedIn && <Header />} */}
                         <div className="content-container">
                             <CustomScrollbars style={{ height: '100vh', width: '100%' }}>
                                 <Switch>
