b4f881f72281881bb0b8739d21cae92985aaf1fc
nguyenvangiang-021002 <117251788+nguyenvangiang-021002@users.noreply.github.com>
2025-06-09 19:18:22 +0700

    Tạo Dữ Liệu Các Cơ Sở Y Tế/Phòng Khám Bệnh

--- Frontend/src/services/userService.js
+++ Frontend/src/services/userService.js
@@ -73,6 +73,9 @@ const getAllSpecialty = () => {
 const getAllDetailSpecialtyById = (data) => {
     return axios.get(`/api/get-detail-specialty-by-id?id=${data.id}&location=${data.location}`)
 }
+const createNewClinic = (data) => {
+    return axios.post('/api/create-new-clinic', data)
+}
 export {
     handleLoginApi,
     getAllUsers,
@@ -92,6 +95,7 @@ export {
     postVerifyBookAppointment,
     createNewSpecialty,
     getAllSpecialty,
-    getAllDetailSpecialtyById
+    getAllDetailSpecialtyById,
+    createNewClinic
 
 }
