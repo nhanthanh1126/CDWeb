aabf245f51de82b4ef3aa1f4705aa1d8a1bc25b7
Truong Thanh Nhan <91606855+nhanthanh1126@users.noreply.github.com>
2025-06-14 05:43:38 +0700

    fix bug

--- Frontend/src/services/userService.js
+++ Frontend/src/services/userService.js
@@ -70,10 +70,18 @@ const createNewSpecialty = (data) => {
 const getAllSpecialty = () => {
     return axios.get(`/api/get-specialty`)
 }
+const getAllClinic = () => {
+    return axios.get(`/api/get-clinic`)
+}
 const getAllDetailSpecialtyById = (data) => {
     return axios.get(`/api/get-detail-specialty-by-id?id=${data.id}&location=${data.location}`)
 }
-
+const getAllDetailClinicById = (data) => {
+    return axios.get(`/api/get-detail-clinic-by-id?id=${data.id}`)
+}
+const createNewClinic = (data) => {
+    return axios.post('/api/create-new-clinic', data)
+}
 const getAllPatientForDoctor = (data) => {
     return axios.get(`/api/get-list-patient-for-doctor?doctorId=${data.doctorId}&date=${data.date}`)
 }
@@ -101,6 +109,9 @@ export {
     createNewSpecialty,
     getAllSpecialty,
     getAllDetailSpecialtyById,
+    createNewClinic,
+    getAllClinic,
+    getAllDetailClinicById,
     getAllPatientForDoctor,
     postSendRemedy
 }
